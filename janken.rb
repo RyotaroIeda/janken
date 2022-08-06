e5e9a9084e00d545b0862a6e22d7e219d54b9cfd
RyotaroIeda <109324447+RyotaroIeda@users.noreply.github.com>
2022-08-06 13:16:27 +0900

    Update janken.rb

--- janken.rb
+++ janken.rb
@@ -1,5 +1,47 @@
- require "./point"
- 
+def point_one #じゃんけん勝った場合
+  puts "あっち向いて〜"
+  puts "0(上)1(下)2(左)3(右)"
+  
+  player_hand = gets.chomp.to_i
+  program_face = rand(4)
+  
+  points = ["上","下","左","右"]
+  
+  puts "ホイ"
+  puts "--------------------"
+  puts "あなた：#{points[player_hand]}"
+  puts "相手：#{points[program_face]}"
+  puts "--------------------"
+  
+  if player_hand == program_face
+    puts "あなたの勝ちです"
+  else
+    janken
+  end
+end
+
+def point_two #じゃんけん負けた場合
+  puts "あっち向いて〜"
+  puts "0(上)1(下)2(左)3(右)"
+  
+  player_face = gets.chomp.to_i
+  program_hand = rand(4)
+  
+  points = ["上","下","左","右"]
+  
+  puts "ホイ"
+  puts "--------------------"
+  puts "あなた：#{points[player_face]}"
+  puts "相手：#{points[program_hand]}"
+  puts "--------------------"
+  
+  if player_face == program_hand
+    puts "あなたの負けです"
+  else
+    janken
+  end
+end
+
  def janken #じゃんけん
     puts "じゃんけん"
     puts "0(グー)1(チョキ)2(パー)3(戦わない)"
@@ -26,3 +68,5 @@
      puts "試合終了です"
     end
  end
+ 
+ janken #アプリスタート
